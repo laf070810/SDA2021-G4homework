5e470d4f120e8afa6251987cbd40f1edf7f7abde
laf <anfeng.li@cern.ch>
2021-12-21 14:32:08 +0800

    add seed position calculation

--- calculate.py
+++ calculate.py
@@ -22,23 +22,19 @@ world_size_z = 1.2 * (calorimeter_size_z + 2 * 5000)
 first_position = -calorimeter_size_z / 2
 
 
-for i in range(1):
-    photon_energy = 0.5 * (i + 1)
+for i in range(6):
+    photon_energy = [1, 2, 5, 10, 50, 100][i]
 
     f = TFile(f'data/B4_{i}.root')
     tr = f.Get('B4')
 
     nf = TFile(f'data/E_and_position_{photon_energy}GeV.root', 'recreate')
     ntr = TTree('E_and_position', '')
-    buffer = [np.zeros(1) for _ in range(4)]
+    buffer = [np.zeros(1) for _ in range(3)]
     ntr.Branch('Esum', buffer[0], 'Esum/D')
     ntr.Branch('shower_x', buffer[1], 'shower_x/D')
     ntr.Branch('shower_y', buffer[2], 'shower_y/D')
-    ntr.Branch('shower_z', buffer[3], 'shower_z/D')
 
-    seed_x = 0
-    seed_y = 0
-    seed_z = 0
     radius = 100
 
     num = 0
@@ -47,35 +43,38 @@ for i in range(1):
             print(f"{num}/{tr.GetEntries()}")
         num += 1
 
-        Esum = 0
-        shower_x = 0
-        shower_y = 0
-        shower_z = 0
-        index = -1
+        index = 0
+        Esum_xy = np.zeros((cell_num_on_x_per_layer, cell_num_on_y_per_layer))
         for iz in range(scintillator_layer_num):
             for iy in range(cell_num_on_y_per_layer):
                 for ix in range(cell_num_on_x_per_layer):
-                    x_position = -calorimeter_size_x / 2 + ix * cell_width_on_x + cell_width_on_x / 2
-                    y_position = -calorimeter_size_y / 2 + iy * cell_width_on_y + cell_width_on_y / 2
-                    z_position = first_position + Fe_thickness + plastic_thickness + iz * \
-                        (scintillator_thickness + gap_thickness) + scintillator_thickness / 2
+                    Esum_xy[ix, iy] += event.Eabs[index]
                     index += 1
 
-                    if ((x_position - seed_x) ** 2 + (y_position - seed_y) ** 2 > radius ** 2):
-                        continue
+        seed_x_index, seed_y_index = np.unravel_index(np.argmax(Esum_xy), Esum_xy.shape)
+        seed_x = -calorimeter_size_x / 2 + seed_x_index * cell_width_on_x + cell_width_on_x / 2
+        seed_y = -calorimeter_size_y / 2 + seed_y_index * cell_width_on_y + cell_width_on_y / 2
 
-                    Esum += event.Eabs[index]
-                    shower_x += x_position * event.Eabs[index]
-                    shower_y += y_position * event.Eabs[index]
-                    shower_z += z_position * event.Eabs[index]
+        Esum = 0
+        shower_x = 0
+        shower_y = 0
+        for iy in range(cell_num_on_y_per_layer):
+            for ix in range(cell_num_on_x_per_layer):
+                x_position = -calorimeter_size_x / 2 + ix * cell_width_on_x + cell_width_on_x / 2
+                y_position = -calorimeter_size_y / 2 + iy * cell_width_on_y + cell_width_on_y / 2
+
+                if ((x_position - seed_x) ** 2 + (y_position - seed_y) ** 2 > radius ** 2):
+                    continue
+
+                Esum += Esum_xy[ix, iy]
+                shower_x += x_position * Esum_xy[ix, iy]
+                shower_y += y_position * Esum_xy[ix, iy]
         shower_x /= Esum
         shower_y /= Esum
-        shower_z /= Esum
 
         buffer[0][0] = Esum
         buffer[1][0] = shower_x
         buffer[2][0] = shower_y
-        buffer[3][0] = shower_z
         ntr.Fill()
 
     ntr.Write()
