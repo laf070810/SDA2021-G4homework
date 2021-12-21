3094d25db71f114f30522e59edfd7763f2cea13c
laf <anfeng.li@cern.ch>
2021-12-21 14:33:10 +0800

    modify data plotting

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 from ROOT import *
 gROOT.ProcessLine(".x ~/lhcbStyle.C")
+gROOT.SetBatch(True)
 
 
 def draw_1Dhists(hists):
@@ -15,27 +16,25 @@ def draw_1Dhists(hists):
         canvas.SaveAs('hists/' + hist.GetName()[1:] + '.pdf')
 
 
-for i in range(20):
-    photon_energy = 0.5 * (i + 1)
+for i in range(6):
+    photon_energy = [1, 2, 5, 10, 50, 100][i]
 
     f = TFile(f'data/E_and_position_{photon_energy}GeV.root')
     tr = f.Get('E_and_position')
 
     nBins = 50
-    hE = TH1F(f"hE_{photon_energy}GeV", "E_{sum} (MeV)", nBins, 0 + 70 * i, 400 + 70 * i)
-    hShowerX = TH1F(f"hShowerX_{photon_energy}GeV", "x (mm)", nBins, -15 + 0.5 * i, 15 - 0.5 * i)
-    hShowerY = TH1F(f"hShowerY_{photon_energy}GeV", "y (mm)", nBins, -15 + 0.5 * i, 15 - 0.5 * i)
-    hShowerZ = TH1F(f"hShowerZ_{photon_energy}GeV", "z (mm)", nBins, -200 + 3 * i, 50 + 1.5 * i)
+    hE = TH1F(f"hE_{photon_energy}GeV", "E_{sum} (MeV)", nBins, -100 + 130 * photon_energy, 100 + 170 * photon_energy)
+    hShowerX = TH1F(f"hShowerX_{photon_energy}GeV", "x (mm)", nBins, -100, 100)
+    hShowerY = TH1F(f"hShowerY_{photon_energy}GeV", "y (mm)", nBins, -100, 100)
 
     num = 0
     for event in tr:
-        if num % 1000 == 0:
+        if num % 100 == 0:
             print(f"{num}/{tr.GetEntries()}")
         num += 1
 
         hE.Fill(event.Esum)
         hShowerX.Fill(event.shower_x)
         hShowerY.Fill(event.shower_y)
-        hShowerZ.Fill(event.shower_z)
 
-    draw_1Dhists([hE, hShowerX, hShowerY, hShowerZ])
+    draw_1Dhists([hE, hShowerX, hShowerY])
